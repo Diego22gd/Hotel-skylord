3a5ea0d71883adc56c3c1c3c66611f8317a41318
Diega12 <diegoabreu655@gmail.com>
2024-11-09 01:14:08 -0400

    models para habitaciones

--- hotelreserva/hotel_backend/reservas/models.py
+++ hotelreserva/hotel_backend/reservas/models.py
@@ -12,3 +12,16 @@ class Reserva(models.Model):
 
     def __str__(self):
         return f'Reserva de {self.client_name} para {self.room_type}'
+
+
+
+class Habitacion(models.Model):
+    status = models.CharField(max_length=50)
+    tipo = models.CharField(max_length=50, blank=True, null=True)  # Tipo de habitación
+    precio = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)  # Precio
+    capacidad = models.IntegerField(blank=True, null=True)  # Capacidad de personas
+    descripcion = models.TextField(blank=True, null=True)  # Descripción adicional
+    creada_en = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Habitación {self.id} - {self.status}"
