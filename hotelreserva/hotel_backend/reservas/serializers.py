1e585eb45264f66687b4122ddea703d4cbfe1b2e
Diega12 <diegoabreu655@gmail.com>
2024-11-09 01:25:42 -0400

    serializer y enpoint
    serializer y endpoint para habitaciones

--- hotelreserva/hotel_backend/reservas/serializers.py
+++ hotelreserva/hotel_backend/reservas/serializers.py
@@ -2,6 +2,12 @@
 # archivo: serializers.py
 from rest_framework import serializers
 from .models import Reserva
+from .models import Habitacion
+
+class HabitacionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Habitacion
+        fields = '__all__'
 
 class ReservaSerializer(serializers.ModelSerializer):
     class Meta:
--- hotelreserva/hotel_backend/reservas/views.py
+++ hotelreserva/hotel_backend/reservas/views.py
@@ -2,10 +2,16 @@
 from rest_framework import viewsets
 from .models import Reserva
 from .serializers import ReservaSerializer
+from .models import Habitacion
+from .serializers import HabitacionSerializer
 
 class ReservaViewSet(viewsets.ModelViewSet):
     queryset = Reserva.objects.all()
     serializer_class = ReservaSerializer
+
+class HabitacionViewSet(viewsets.ModelViewSet):
+    queryset = Habitacion.objects.all()
+    serializer_class = HabitacionSerializer
 
 
 
